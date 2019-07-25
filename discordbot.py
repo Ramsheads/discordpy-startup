bdf9296d15c9f90d0f1f374d97ba1f6ec5ca7429
Ramsheads <hatchet_qoo@yahoo.co.jp>
2019-07-25 13:15:31 +0900

    Process Command and Add debug_info command

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,12 @@
 from discord.ext import commands
 import os
 import traceback
+import textwrap as tw
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+COMMAND_PREFIX="/"
+TOKEN_ENVIRON="DISCORD_BOT_TOKEN"
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)
+token = os.environ[TOKEN_ENVIRON]
 # VoiceClientつくって bot.voice_clients に入れる？
 
 @bot.event
@@ -18,13 +21,22 @@ async def on_message(ctx):
     if "ポン" in ctx.content:
         await ctx.channel.send('ポンにゃ！')
 
+    bot.process_commands(ctx)
+
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong\nvoice_clients: {}'.format(bot.voice_clients))
+    await ctx.send('pong')
 
+@bot.command()
+async def debug_info(ctx):
+    await ctx.send(tw.dedent("""
+    voice_clients: {vc}
+    """.format(
+        vc=bot.voice_clients
+    ))
 
 bot.run(token)
