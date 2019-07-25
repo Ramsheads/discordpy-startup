450182dfccd6f29dfeed6c2eb8b41218e22cacd5
Ramsheads <hatchet_qoo@yahoo.co.jp>
2019-07-25 12:53:03 +0900

    Update to check voice_clients contents

--- discordbot.py
+++ discordbot.py
@@ -4,16 +4,27 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+# VoiceClientつくって bot.voice_clients に入れる？
 
+@bot.event
+async def on_ready():
+    print("Start Ponnya Chan...")
+
+@bot.event
+async def on_message(ctx):
+    if ctx.author == bot.user:
+        return
+
+    if "ポン" in ctx.content:
+        await ctx.channel.send('ポンにゃ！')
 
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
-
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('pong\nvoice_clients: {}'.format(bot.voice_clients))
 
 
 bot.run(token)
