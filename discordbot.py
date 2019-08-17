8043ddb276074d7a01afdedfe82561389ab9c440
Ramsheads <hatchet_qoo@yahoo.co.jp>
2019-08-17 18:49:34 +0900

    Add class VoiceSource and update for sukan and suhu

--- discordbot.py
+++ discordbot.py
@@ -3,120 +3,171 @@ from discord.ext import commands
 import os
 import traceback
 import textwrap as tw
+import re
 
 COMMAND_PREFIX="$$"
 TOKEN_ENVIRON="DISCORD_BOT_TOKEN"
 token = os.environ[TOKEN_ENVIRON]
-PON_WAV="ponn.wav"
-PON_WAV_FILE = open(PON_WAV, "rb")
-PON_SOURCE=discord.PCMVolumeTransformer(
-        discord.PCMAudio(PON_WAV_FILE), volume=0.25)
-JOIN_WAV="join.wav"
-JOIN_WAV_FILE = open(JOIN_WAV, "rb")
-JOIN_SOURCE=discord.PCMVolumeTransformer(
-        discord.PCMAudio(JOIN_WAV_FILE), volume=0.25)
-RON_WAV="ron.wav"
-RON_WAV_FILE = open(RON_WAV, "rb")
-RON_SOURCE=discord.PCMVolumeTransformer(
-        discord.PCMAudio(RON_WAV_FILE), volume=0.25)
-KAN_WAV="kan.wav"
-KAN_WAV_FILE = open(KAN_WAV, "rb")
-KAN_SOURCE=discord.PCMVolumeTransformer(
-        discord.PCMAudio(KAN_WAV_FILE), volume=0.25)
-CHEE_WAV="chee.wav"
-CHEE_WAV_FILE = open(CHEE_WAV, "rb")
-CHEE_SOURCE=discord.PCMVolumeTransformer(
-        discord.PCMAudio(CHEE_WAV_FILE), volume=0.25)
-PEI_WAV="pei.wav"
-PEI_WAV_FILE = open(PEI_WAV, "rb")
-PEI_SOURCE=discord.PCMVolumeTransformer(
-        discord.PCMAudio(PEI_WAV_FILE), volume=0.25)
-REACH_WAV="reach.wav"
-REACH_WAV_FILE = open(REACH_WAV, "rb")
-REACH_SOURCE=discord.PCMVolumeTransformer(
-        discord.PCMAudio(REACH_WAV_FILE), volume=0.25)
-TUMO_WAV="tumo.wav"
-TUMO_WAV_FILE = open(TUMO_WAV, "rb")
-TUMO_SOURCE=discord.PCMVolumeTransformer(
-        discord.PCMAudio(TUMO_WAV_FILE), volume=0.25)
+#TODO: Need to make them class
+
+class VoiceSource:
+    def __init__(self, bot, path, default_vol=0.25, pattern="", msg=""):
+        self.bot=bot
+
+        if not isinstance(path, str):
+            raise TypeError("path should be str")
+        self.path=path
+
+        if not isinstance(default_vol, (int, float)):
+            raise TypeError("default_vol should be int or float")
+        if default_vol < 0 or default_vol > 1:
+            raise ValueError("default_vol should be 0.0 ~ 1.0")
+        self.default_vol=default_vol
+
+        if not isinstance(pattern, str):
+            raise TypeError("pattern should be str")
+        self.pattern=pattern
+    
+        if not isinstance(msg, str):
+            raise TypeError("msg should be str")
+        self.msg=msg
+
+        self.__set_src__()
+        self.__set_regex__()
+    
+    def __set_src__(self):
+        self._file=open(self.path, "rb")
+        self._src=discord.PCMVolumeTransformer(
+            discord.PCMAudio(self._file),
+            volume=self.default_vol
+        )
+    
+    def __set_regex__(self):
+        self._regex=re.compile(self.pattern)
+
+    #FIXME: Need to use on multiple servers
+    def play(self):
+        for vc in self.bot.voice_clients:
+            if vc.is_playing():
+                vc.stop()
+            self._src.cleanup()
+            self._file.seek(0)
+            vc.play(
+                self._src,
+                after=lambda e: print(
+                    "ERROR {}: {}".format(self.path, e)))
+    
+    def say(self, ctx):
+        if self.msg:
+            await ctx.channel.send(self.msg)
+    
+    def check_play(self, ctx):
+        if re.search(self._regex, ctx.content):
+            try:
+                self.say(ctx)
+                self.play()
+            except Exception as e:
+                await ctx.channel.send(
+                    "ERROR on {}: {}".format(ctx.content, e))
+            return True
+        return False
 
 IS_DEBUG=False
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
+vs_pon = VoiceSource(bot,
+    "ponn.wav", pattern=r'ポン|ぽん', msg='ポンにゃ！')
+vs_ron = VoiceSource(bot,
+    "ron.wav", pattern=r'ロン|ろん', msg='ロンにゃ！')
+vs_kan = VoiceSource(bot,
+    "kan.wav", pattern=r'カン|かん', msg='カンにゃ！')
+vs_sukan = VoiceSource(bot,
+    "sukan.wav", pattern=r'カン|かん', msg='スーカンながれにゃー……')
+SUKAN_COUNT=0
+vs_chee = VoiceSource(bot,
+    "chee.wav", pattern=r'チー|ちー', msg='チーにゃ！')
+vs_pei = VoiceSource(bot,
+    "pei.wav", pattern=r'北|ペー|ぺー', msg='ペーにゃ！')
+vs_suhu = VoiceSource(bot,
+    "suhu.wav", pattern=r'北|ペー|ぺー', msg='スーフーにゃんだにゃー……')
+SUHU_COUNT=0
+vs_reach = VoiceSource(bot,
+    "reach.wav", pattern=r'リーチ|りーち', msg='リーチにゃ！')
+vs_tumo = VoiceSource(bot,
+    "tumo.wav", pattern=r'ツモ|つも', msg='ツモにゃ！')
+
 # Yet implemented event handler
 # on_member_join(member): ポンにゃテロ
 # discord.on_voice_state_update(member, before, after):　来たにゃ！
-
-def is_voice_connected():
-    if bot.voice_clients:
-        return True
-    return False
 
 @bot.event
 async def on_ready():
     print("Start Ponnya Chan...")
 
-def __play(voice_client, source, wav, msg):
-    if voice_client.is_playing():
-        voice_clinet.stop()
-    source.cleanup()
-    wav.seek(0)
-    voice_client.play(source,
-            after=lambda e: print("ERROR {}: {}".format(msg, e)))
+def clear_sukan():
+    global SUKAN_COUNT
+    SUKAN_COUNT = 0
 
-def play(source, wav, msg):
-    if is_voice_connected():
-        for voice_client in bot.voice_clients:
-            __play(voice_client, source, wav, msg)
+def is_sukan():
+    global SUKAN_COUNT
+    SUKAN_COUNT += 1
+    if SUKAN_COUNT != 0 and SUKAN_COUNT % 4 == 0:
+        clear_sukan()
+        return True
+    return False
+
+def clear_suhu():
+    global SUHU_COUNT
+    SUHU_COUNT = 0
+
+def is_suhu():
+    global SUHU_COUNT
+    SUHU_COUNT += 1
+    if SUHU_COUNT != 0 and SUHU_COUNT % 4 == 0:
+        clear_suhu()
+        return True
+    return False
+
+def clear_all_counter():
+    clear_suhu()
+    clear_sukan()
 
 @bot.event
 async def on_message(ctx):
+    global SUKAN_COUNT
     if ctx.author == bot.user:
         return
-
-    if "ポン" in ctx.content:
-        await ctx.channel.send('ポンにゃ！')
-        try:
-            play(PON_SOURCE, PON_WAV_FILE, "on_message")
-        except Exception as e:
-            await ctx.channel.send("ponn error: {}".format(e))
-
-    if "ロン" in ctx.content:
-        await ctx.channel.send('ロンにゃ！')
-        play(RON_SOURCE, RON_WAV_FILE, "on_message")
-
-    if "カン" in ctx.content:
-        await ctx.channel.send('カンにゃ！')
-        play(KAN_SOURCE, KAN_WAV_FILE, "on_message")
-
-    if "チー" in ctx.content:
-        await ctx.channel.send('チーにゃ！')
-        play(CHEE_SOURCE, CHEE_WAV_FILE, "on_message")
-
-    if "北" in ctx.content:
-        await ctx.channel.send('ぺーにゃ！')
-        play(PEI_SOURCE, PEI_WAV_FILE, "on_message")
-
-    if "リーチ" in ctx.content:
-        await ctx.channel.send('リーチにゃ！')
-        play(REACH_SOURCE, REACH_WAV_FILE, "on_message")
-
-    if "ツモ" in ctx.content:
-        await ctx.channel.send('ツモにゃ！')
-        play(TUMO_SOURCE, TUMO_WAV_FILE, "on_message")
-
+    
     try:
-        await bot.process_commands(ctx)
-    except KeyboardInterrupt:
-        exit()
+        if vs_pon.check_play(ctx):
+            pass
+        elif vs_chee.check_play(ctx):
+            pass
+        elif vs_ron.check_play(ctx):
+            clear_all_counter()
+        elif vs_tumo.check_play(ctx):
+            clear_all_counter()
+        elif vs_reach.check_play(ctx):
+            pass
+        elif vs_kan.check_play(ctx):
+            if is_sukan():
+                vs_sukan.say(ctx)
+                vs_sukan.play()
+                clear_all_counter()
+        elif vs_pei.check_play(ctx):
+            if is_suhu():
+                vs_suhu.say(ctx)
+                vs_suhu.play()
+                clear_all_counter()
+        else:
+            try:
+                await bot.process_commands(ctx)
+            except KeyboardInterrupt:
+                exit()
     except Exception as e:
         await ctx.channel.send(
                 "Ponnya Chan raised Exception:\n\t{}".format(e))
-
-@bot.event
-async def on_member_join(member):
-    play(JOIN_SOURCE, JOIN_WAV_FILE, "on_member_join")
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -129,14 +180,16 @@ async def ping(ctx):
 @bot.command()
 async def connect(ctx):
     # VoiceClient connect
+    # Try to connect a voice channel that author joined
     author = ctx.message.author
     try:
         channel = author.voice.channel
     except:
         channel = None
+    #FIXME: Need to connect multiple voice channels over the servers
     if channel != None:
         try:
-            if is_voice_connected():
+            if bot.voice_clients:
                 for vc in bot.voice_clients:
                     vc.move_to(channel)
             else:
@@ -154,6 +207,7 @@ async def connect(ctx):
         
 @bot.command()
 async def disconnect(ctx):
+    #FIXME: Need to disconnect a single channel based on ctx
     #VoiceClient disconnect
     for voice_client in bot.voice_clients:
         await voice_client.disconnect()
@@ -164,10 +218,12 @@ async def debug_info(ctx):
         return
     await ctx.send(tw.dedent("""
     voice_clients: {vc}
-    source: {src} {src_dir}
+    sukan: {sukan}
+    suhu: {suhu}
     """.format(
         vc=bot.voice_clients,
-        src=SOURCE, src_dir=dir(SOURCE)
+        sukan=SUKAN_COUNT,
+        suhu=SUHU_COUNT,
     )))
 
 bot.run(token)
